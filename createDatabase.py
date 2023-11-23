b6694b6f4877477fea35e9575541d516c572631c
Aaron Satko <agsatko@gmail.com>
2023-11-23 13:07:03 -0500

    it works but need to change obj_ID

--- createDatabase.py
+++ createDatabase.py
@@ -29,13 +29,14 @@ try:
 
     print("Database connection successful.")
     cursor = dbconn.cursor()
-    
+
     # Drop tables if they exist
-    cursor.execute("DROP TABLE IF EXISTS celestial_object;")
+    cursor.execute("DROP TABLE IF EXISTS celestial_spectroscopic_data;")
     cursor.execute("DROP TABLE IF EXISTS celestial_photometric_data;")
     cursor.execute("DROP TABLE IF EXISTS celestial_observation_runs;")
-    cursor.execute("DROP TABLE IF EXISTS celestial_spectroscopic_data;")
+    cursor.execute("DROP TABLE IF EXISTS celestial_object;")
     cursor.execute("DROP TABLE IF EXISTS celestial_class;")
+
 
     dbconn.commit()
 
@@ -50,7 +51,8 @@ try:
     
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS celestial_object (
-            obj_ID BIGINT PRIMARY KEY,
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            obj_ID BIGINT,
             alpha DECIMAL(17, 12),
             delta DECIMAL(17, 12),
             class_id INT,
@@ -59,20 +61,20 @@ try:
     """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS celestial_photometric_data (
+            id INT PRIMARY KEY AUTO_INCREMENT,
             obj_ID BIGINT,
             u DECIMAL(10, 5),
             g DECIMAL(10, 5),
             r DECIMAL(10, 5),
             i DECIMAL(10, 5),
             z DECIMAL(10, 5),
-            PRIMARY KEY (obj_ID),
-            FOREIGN KEY (obj_ID) REFERENCES celestial_object(obj_ID)
+            FOREIGN KEY (id) REFERENCES celestial_object(id)
         );
     """)
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS celestial_observation_runs (
             run_ID INT PRIMARY KEY,
-            rereun_ID INT,
+            rerun_ID INT,
             cam_col INT,
             field_ID INT
         );
@@ -80,61 +82,63 @@ try:
     
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS celestial_spectroscopic_data (
-            spec_obj_ID VARCHAR(50) PRIMARY KEY,
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            spec_obj_ID VARCHAR(50),
             obj_ID BIGINT,
             plate INT,
             MJD INT,
             fiber_ID INT,
-            FOREIGN KEY (obj_ID) REFERENCES celestial_object(obj_ID)
+            FOREIGN KEY (id) REFERENCES celestial_object(id)
         );
     """)
 
     dbconn.commit()
 
     # Insert class names
+    '''
     cursor.execute("INSERT INTO celestial_class (class_name) VALUES ('GALAXY'), ('STAR'), ('QSO');")
     dbconn.commit()
-
-    # Insert data in batches
+    '''
+    
     def insert_batch(table_name, insert_query, data, batch_size=1000):
-        with tqdm(total=len(data), desc=f"Inserting into {table_name}") as pbar:
-            for i in range(0, len(data), batch_size):
-                batch_data = data[i:i + batch_size]
-                cursor.executemany(insert_query, batch_data)
-                dbconn.commit()
-                pbar.update(len(batch_data))
+        for i in tqdm(range(0, len(data), batch_size), desc=f"Inserting into {table_name}"):
+            batch_data = data[i:i + batch_size]
+            cursor.executemany(insert_query, batch_data)
+            dbconn.commit()
 
-    # Preparing data for celestial_object
-    celestial_object_data = []
-    for _, row in df.iterrows():
-        class_id_query = f"SELECT class_id FROM celestial_class WHERE class_name = '{row['class']}'"
-        cursor.execute(class_id_query)
-        class_id = cursor.fetchone()[0]
-        celestial_object_data.append((row['obj_ID'], row['alpha'], row['delta'], class_id))
+    # Insert unique class names into celestial_class
+    class_names = df['class'].unique()
+    class_data = [(name,) for name in class_names]
+    insert_class_query = "INSERT INTO celestial_class (class_name) VALUES (%s);"
+    insert_batch("celestial_class", insert_class_query, class_data)
 
-    # Batch insert data into celestial_object
-    insert_celestial_object_query = "INSERT INTO celestial_object (obj_ID, alpha, delta, class_id) VALUES (%s, %s, %s, %s)"
-    insert_batch("celestial_object", insert_celestial_object_query, celestial_object_data)
+    # Map class names to class_ids
+    cursor.execute("SELECT class_id, class_name FROM celestial_class;")
+    class_map = {name: id for id, name in cursor.fetchall()}
 
-    # Preparing data for celestial_photometric_data
+    # Insert data into celestial_object
+    celestial_data = [(row['obj_ID'], row['alpha'], row['delta'], class_map[row['class']]) for _, row in df.iterrows()]
+    insert_celestial_query = """INSERT INTO celestial_object (obj_ID, alpha, delta, class_id)
+                                VALUES (%s, %s, %s, %s);"""
+    insert_batch("celestial_object", insert_celestial_query, celestial_data)
+
+    # Insert data into celestial_photometric_data
     photometric_data = [(row['obj_ID'], row['u'], row['g'], row['r'], row['i'], row['z']) for _, row in df.iterrows()]
-
-    # Batch insert data into celestial_photometric_data
-    insert_photometric_query = "INSERT INTO celestial_photometric_data (obj_ID, u, g, r, i, z) VALUES (%s, %s, %s, %s, %s, %s)"
+    insert_photometric_query = """INSERT INTO celestial_photometric_data (obj_ID, u, g, r, i, z)
+                                  VALUES (%s, %s, %s, %s, %s, %s);"""
     insert_batch("celestial_photometric_data", insert_photometric_query, photometric_data)
-
-    # Preparing data for celestial_observation_runs
-    observation_runs_data = [(row['run_ID'], row['rerun_ID'], row['cam_col'], row['field_ID']) for _, row in df.iterrows()]
-
-    # Batch insert data into celestial_observation_runs
-    insert_observation_query = "INSERT INTO celestial_observation_runs (run_ID, rereun_ID, cam_col, field_ID) VALUES (%s, %s, %s, %s)"
-    insert_batch("celestial_observation_runs", insert_observation_query, observation_runs_data)
-
-    # Preparing data for celestial_spectroscopic_data
+    
+    # Insert data into celestial_observation_runs
+    observation_run_data = [(row['run_ID'], row['rerun_ID'], row['cam_col'], row['field_ID']) for _, row in df.iterrows()]
+    insert_observation_run_query = """INSERT INTO celestial_observation_runs (run_ID, rerun_ID, cam_col, field_ID)
+                                    VALUES (%s, %s, %s, %s)
+                                    ON DUPLICATE KEY UPDATE rerun_ID=VALUES(rerun_ID), cam_col=VALUES(cam_col), field_ID=VALUES(field_ID);"""
+    insert_batch("celestial_observation_runs", insert_observation_run_query, observation_run_data)
+    
+    # Insert data into celestial_spectroscopic_data
     spectroscopic_data = [(row['spec_obj_ID'], row['obj_ID'], row['plate'], row['MJD'], row['fiber_ID']) for _, row in df.iterrows()]
-
-    # Batch insert data into celestial_spectroscopic_data
-    insert_spectroscopic_query = "INSERT INTO celestial_spectroscopic_data (spec_obj_ID, obj_ID, plate, MJD, fiber_ID) VALUES (%s, %s, %s, %s, %s)"
+    insert_spectroscopic_query = """INSERT INTO celestial_spectroscopic_data (spec_obj_ID, obj_ID, plate, MJD, fiber_ID)
+                                    VALUES (%s, %s, %s, %s, %s);"""
     insert_batch("celestial_spectroscopic_data", insert_spectroscopic_query, spectroscopic_data)
 
 

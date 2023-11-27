1c7219b1b4bda77cdfc097c922c2a9f4d0f822a1
Aaron Satko <120073926+aaronsatko@users.noreply.github.com>
2023-11-26 23:46:58 -0500

    queries generate plots based on user input

--- query.py
+++ query.py
@@ -1,6 +1,9 @@
 import pymysql
 import configparser
 import logging
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
 # Set up logging
 logging.basicConfig(filename='database_errors.log', level=logging.ERROR, 
@@ -15,6 +18,101 @@ dbuser = config['csc']['dbuser']
 dbpw = config['csc']['dbpw']
 dbschema = config['csc']['dbschema']
 
+# Create directory for plots if it doesn't exist
+plots_dir = "query_generated_plots"
+os.makedirs(plots_dir, exist_ok=True)
+
+# Visualization functions for each query
+# Query 1
+def plot_avg_stddev_by_class(result):
+    classes = [row[0] for row in result]
+    averages = { 'U': [row[1] for row in result], 'G': [row[3] for row in result], 'R': [row[5] for row in result], 'I': [row[7] for row in result], 'Z': [row[9] for row in result] }
+    stddevs = { 'U': [row[2] for row in result], 'G': [row[4] for row in result], 'R': [row[6] for row in result], 'I': [row[8] for row in result], 'Z': [row[10] for row in result] }
+    
+    # Creating the plot
+    plt.figure(figsize=(12, 6))
+    for i, (filter_name, avg_values) in enumerate(averages.items()):
+        x_positions = [x + i*0.1 for x in range(len(classes))]  # Offset x positions for each filter
+        plt.bar(x_positions, avg_values, width=0.1, yerr=stddevs[filter_name], label=f'{filter_name} Filter')
+    
+    plt.xticks(range(len(classes)), classes)
+    plt.xlabel('Class')
+    plt.ylabel('Average Value')
+    plt.title('Average and Stddev of Filters by Class')
+    plt.legend()
+    
+    # save
+    plot_filename = os.path.join(plots_dir, 'avg_stddev_by_class.png')
+    plt.savefig(plot_filename)
+    plt.show()
+
+# Query 2
+def plot_highest_redshift(result):
+    object_ids = [row[0] for row in result]
+    redshifts = [row[-2] for row in result]
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=object_ids, y=redshifts)
+    plt.xticks(rotation=45)
+    plt.xlabel('Object ID')
+    plt.ylabel('Redshift')
+    plt.title('Top 10 Celestial Objects with Highest Redshift')
+    plt.tight_layout()
+
+    plot_filename = os.path.join(plots_dir, 'highest_redshift.png')
+    plt.savefig(plot_filename)
+    plt.show()
+        
+# Query 3
+def plot_distribution_across_plates(result):
+    plates = [row[0] for row in result]
+    observation_counts = [row[1] for row in result]
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=plates, y=observation_counts, order=plates)
+    plt.xticks(rotation=45)
+    plt.xlabel('Plate')
+    plt.ylabel('Number of Observations')
+    plt.title('Distribution of Observations Across Plates')
+    plt.tight_layout()
+
+    plot_filename = os.path.join(plots_dir, 'plates_distribution.png')
+    plt.savefig(plot_filename)
+    plt.show()
+
+
+# Query 4
+def plot_avg_redshift_comparison(result):
+    classes = [row[0] for row in result]
+    avg_redshifts = [row[1] for row in result]
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=classes, y=avg_redshifts)
+    plt.xlabel('Class')
+    plt.ylabel('Average Redshift')
+    plt.title('Average Redshift Values Comparison')
+    plt.tight_layout()
+
+    plot_filename = os.path.join(plots_dir, 'avg_redshift_comparison.png')
+    plt.savefig(plot_filename)
+    plt.show()
+    
+# Query 5
+def plot_class_count_and_avg_values(result):
+    classes = [row[0] for row in result]
+    frequencies = [row[1] for row in result]
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=classes, y=frequencies)
+    plt.xlabel('Class')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Each Class')
+    plt.tight_layout()
+
+    plot_filename = os.path.join(plots_dir, 'class_frequency.png')
+    plt.savefig(plot_filename)
+    plt.show()
+
 # Predefined queries
 predefined_queries = {
     1: ("Average and Standard Deviation for Each Filter by Class",
@@ -27,7 +125,7 @@ predefined_queries = {
         """SELECT *, plate FROM celestial_observations ORDER BY redshift DESC LIMIT 10"""),
     3: ("Distribution of Observations Across Different Plates",
         """SELECT plate, COUNT(*) AS observation_count, AVG(redshift) AS avg_redshift 
-           FROM celestial_observations GROUP BY plate ORDER BY observation_count DESC"""),
+           FROM celestial_observations GROUP BY plate ORDER BY observation_count DESC LIMIT 100"""),
     4: ("Average Redshift Values Comparison",
         """SELECT class, AVG(redshift) AS average_redshift, MIN(redshift) AS min_redshift, 
            MAX(redshift) AS max_redshift FROM celestial_observations GROUP BY class"""),
@@ -43,7 +141,8 @@ def select_query():
         print(f"{key}: {predefined_queries[key][0]}")
     choice = int(input("Enter the number of the query you want to execute: "))
     print("")
-    return predefined_queries.get(choice, (None, None))[1]
+    selected_query = predefined_queries.get(choice, (None, None))[1]
+    return choice, selected_query
 
 def format_and_print_result(result):
     for row in result:
@@ -57,13 +156,21 @@ try:
     print("Database connection successful.")
     cursor = dbconn.cursor()
 
-    query = select_query()
+    choice, query = select_query()
     if query:
         cursor.execute(query)
         result = cursor.fetchall()
         format_and_print_result(result)
-    else:
-        print("Invalid query selection.")
+        if choice == 1:
+            plot_avg_stddev_by_class(result)
+        elif choice == 2:
+            plot_highest_redshift(result)
+        elif choice == 3:
+            plot_distribution_across_plates(result)
+        elif choice == 4:
+            plot_avg_redshift_comparison(result)
+        elif choice == 5:
+            plot_class_count_and_avg_values(result)
 
 except pymysql.MySQLError as e:
     error_message = f"Error connecting to the database: {e}"
